d1c2791952cbf685f7a1b49b2e6836601d24bdae
minkyuseo <mk4236@naver.com>
2022-03-12 12:45:19 +0900

    FormView Create 구현

--- board/urls.py
+++ board/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from board.views import BoardCreateView
+from board.views import BoardCreateView, BoardDetailView
 
 urlpatterns = [
-    path('create/', BoardCreateView.as_view())
+    path('create/', BoardCreateView.as_view()),
+    path('detail/<int:pk>/', BoardDetailView.as_view())
 ]
--- board/views.py
+++ board/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, DetailView
 
 from board.forms import BoardCreateForm
 from board.models import Board
@@ -16,3 +16,8 @@ class BoardCreateView(FormView):
         )
         board.save()
         return super().form_valid(form)
+
+
+class BoardDetailView(DetailView):
+    template_name = "board_detail.html"
+    model = Board  # URLConf에서 pk 파라미터의 값을 활용하여 자동으로DB를 조회한다
