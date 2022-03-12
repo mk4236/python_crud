a2fcf46d4717bb3809779f822ad37182d4ceb540
minkyuseo <mk4236@naver.com>
2022-03-12 16:06:45 +0900

    ListView Pagination 적용

--- board/urls.py
+++ board/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from board.views import BoardCreateView, BoardDetailView
+from board.views import BoardCreateView, BoardDetailView, BoardListView
 
 urlpatterns = [
     path('create/', BoardCreateView.as_view()),
+    path('list/', BoardListView.as_view()),
     path('detail/<int:pk>/', BoardDetailView.as_view())
 ]
--- board/views.py
+++ board/views.py
@@ -1,23 +1,47 @@
-from django.views.generic import FormView, DetailView
+from django.shortcuts import redirect
+from django.views.generic import FormView, DetailView, ListView
 
 from board.forms import BoardCreateForm
 from board.models import Board
+from user.models import User
 
 
 class BoardCreateView(FormView):
-    template_name = "board_create.html"
     form_class = BoardCreateForm
-    success_url = "/board/create"
+    template_name = "board_create.html"
+    success_url = "/board/create/"
 
     def form_valid(self, form):
+        # user 정보 취득
+        user_id = self.request.session.get('user_id')
+        if user_id:
+            try:
+                user = User.objects.get(pk=user_id)
+            except User.DoesNotExist:
+                return redirect("/user/login/")
+        else:
+            return redirect("/user/logout/")
+
         board = Board(
             title=form.data.get('title'),
-            contents=form.data.get('contents')
+            contents=form.data.get('contents'),
+            insert_user=user
         )
         board.save()
         return super().form_valid(form)
 
 
 class BoardDetailView(DetailView):
-    template_name = "board_detail.html"
     model = Board  # URLConf에서 pk 파라미터의 값을 활용하여 자동으로DB를 조회한다
+    template_name = "board_detail.html"
+    context_object_name = "board"
+
+
+class BoardListView(ListView):
+    model = Board
+    template_name = "board_list.html"
+    context_object_name = "board_list"
+    ordering = "-id"
+    paginate_by = 5
+    paginate_orphans = 1
+    page_kwarg = "p"
